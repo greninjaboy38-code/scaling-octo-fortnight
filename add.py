3a82ab817a4457208d16a4bc03c9a540abb951eb
greninjaboy38-code <greninjaboy38@gmail.com>
2026-01-19 18:52:16 +0400

    Add files via upload

---
+++ add.py
@@ -0,0 +1,11 @@
+class A:
+    def __init__(self, a):
+        self.a = a
+    def __lt__(self, other):
+        if(self.a<other.a):
+            return "ob1 is less than ob2"
+        else:
+            return "ob2 is less than ob1"
+ob1 = A(2)
+ob2 = A(3)
+print(ob1<ob2)
